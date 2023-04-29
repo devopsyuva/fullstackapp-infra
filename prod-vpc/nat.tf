67c2cf5d3bf20e594b630517cacb1a327f244df9
sudheer devops <sudheerdevops@gmail.com>
2023-04-29 22:06:07 +0530

    [Fix EIP name] terraform_apply vpc

--- prod-vpc/nat.tf
+++ prod-vpc/nat.tf
@@ -1,7 +1,7 @@
 resource "aws_nat_gateway" "private_nat" {
-  allocation_id = aws_eip.vpt_eip.id
+  allocation_id = aws_eip.prod_eip.id
   subnet_id     = aws_subnet.public_subnet.0.id
-  depends_on    = [aws_eip.vpt_eip]
+  depends_on    = [aws_eip.prod_eip]
 
   tags = merge(
     local.common_tags,
