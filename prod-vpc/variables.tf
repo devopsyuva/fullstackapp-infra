71b17b5926f38cf47ff916124d106c336b844e0b
sudheer devops <sudheerdevops@gmail.com>
2023-05-15 18:32:10 +0530

    [az-1c subnet] terraform_apply all

--- prod-vpc/variables.tf
+++ prod-vpc/variables.tf
@@ -14,12 +14,12 @@ locals {
   public_subnets = {
     "${var.aws_region}a" = "192.168.0.0/24"
     "${var.aws_region}b" = "192.168.2.0/24"
-    "${var.aws_region}c" = "192.168.4.0/24"
+    #"${var.aws_region}c" = "192.168.4.0/24"
   }
   private_subnets = {
     "${var.aws_region}a" = "192.168.1.0/24"
     "${var.aws_region}b" = "192.168.3.0/24"
-    "${var.aws_region}c" = "192.168.5.0/24"
+    #"${var.aws_region}c" = "192.168.5.0/24"
   }
 }
 
